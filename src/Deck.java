adcb5a0cd87eb9af53da960c6b9e66f1cd38f8d1
Aiden Rocha <126905791+Arocha09@users.noreply.github.com>
2025-01-11 17:39:12 -0500

    Deck Class and Move Enumeration

--- src/Deck.java
+++ src/Deck.java
@@ -1,9 +1,48 @@
 import java.util.ArrayList;
 
 public class Deck {
-    private ArrayList<Card> cards;
+    private Card[] deck;
+    private int top = 0;
+
+
 
     public Deck() {
-        this.cards = new ArrayList<Card>();
+        deck = new Card[52];
+
+        int i = 0;
+        for(int r = 1; r <= 13; r++) {
+            for(Card.Suit s : Card.Suit.values()) {
+                deck[i] = new Card(r, s);
+                i++;
+            }
+        }
+
+        shuffle();
+    }
+
+    public void shuffle() {
+        for(int i = 0; i < deck.length-1; i++) {
+            int r = (int)(Math.random()*(52-i))+i;
+
+            Card temp = deck[i];
+            deck[i] = deck[r];
+            deck[r] = temp;
+        }
+    }
+    public int cardsLeft(){
+        return deck.length - top;
+    }
+
+    public Card dealTopCard(){
+        if(top > = deck.length){
+            restockDeck();
+        }
+        top++;
+        return deck[top - 1];
+    }
+
+    public void restockDeck() {
+        shuffle();
+        top = 0;
     }
 }
