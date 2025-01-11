b87deb2f030f403b409e74d8aa7d0539ea58de81
Aiden Rocha <126905791+Arocha09@users.noreply.github.com>
2025-01-11 17:48:56 -0500

    Initial commit complete of overall code

--- src/DeckHolder.java
+++ src/DeckHolder.java
@@ -1,16 +1,45 @@
 import java.util.ArrayList;
 
 public class DeckHolder {
-    private ArrayList<Deck> decks;
+   private Deck[] decks;
+   int count = 0;
+   int deckNum = 0;
+   int cards = 0;
 
-    public DeckHolder() {}
 
-    public Card dealTopCard(){
+    public DeckHolder(int numDecks) {
+        decks = new Deck[numDecks];
+        for(int i = 0; i < numDecks; i++) {
+            decks[i] = new Deck();
+        }
 
     }
 
-    public int cardsLeft(){
+    public Card dealTopCard(){
+        if (count == 52){
+            deckNum += 1;
+        }
+        count++;
 
+        if(deckNum >= decks.length){
+            restoreDecks();
+        }
+        return decks[deckNum].dealTopCard();
+    }
+
+    public void restoreDecks(){
+        for(int i = 0; i < decks.length; i++){
+            decks[i].shuffle();
+        }
+        count = 0;
+        decknum = 0;
+    }
+    public int cardsLeft(){
+        int cards = 0;
+        for(int i = 0; i < decks.length; i++){
+            cards += decks[i].cardsLeft();
+        }
+        return cards;
     }
 
 }
---
+++ src/MyBlackjackPlayer.java
@@ -0,0 +1,19 @@
+
+public class MyBlackjackPlayer extends BlackjackPlayer{
+    @Override
+    public int getBet(){return 10;}
+
+    @Override
+    public Move getMove(){
+        if(this.handScore() <= 16) return Move.HIT;
+        return Move.STAY;
+    }
+
+    @Override
+    public void handOver(Card[] dealerHand){
+        int value = 0;
+        for(int i = 0; i<dealerHand.length; i++){
+            value += dealerHand[i].getRank();
+        }
+    }
+}
