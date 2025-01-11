bac33e3a4e922d944ace7cb20f0720636830c0e5
Aiden Rocha <126905791+Arocha09@users.noreply.github.com>
2025-01-11 17:31:21 -0500

    Card and Main Classes started

--- src/Card.java
+++ src/Card.java
@@ -1,19 +1,79 @@
 
 public class Card {
+    public enum Suit{
+        Hearts, Diamonds, Clubs, Spades;
+    }
+
     private String suit;
-    private int value;
     private int rank;
 
-    public Card(String suit, int value) {
-        this.suit = suit;
-        this.value = value;
+    public Card(){
+        this.suit = Suit.Spades;
+        this.rank = 1;
+    }
+    public Card(String suit, int rank) {
+       this.setRank(rank);
+       setSuit(suit);
+    }
+
+    public int getRank(){return this.rank;}
+
+    public String getSuit(){return this.suit;}
+
+    private void setRank(int newRank){
+        if(newRank < 1 || newRank > 13){
+            return;
+        }
+        this.rank = newRank;
+    }
+
+    private void setSuit(Suit newSuit){
+        this.suit = newSuit;
+    }
+
+    public String toString(){
+        String rank = "";
+        switch(this.rank) {
+            case 1:
+                rank = "Ace";
+                break;
+            case 11:
+                rank = "Jack";
+                break;
+            case 12:
+                rank = "Queen";
+                break;
+            case 13:
+                rank = "King";
+                break;
+            default:
+                rank = "" + this.rank; //number and rank the same
+                break;
+        }
+
+        return rank + " of " + this.suit;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o){
+            return true;
+        }
+        if(o == null){
+            return false;
+        }
+        if(!(o instanceof Card)){
+            return false;
+        }
+        Card otherC = (Card)o;
+
+        return otherC.rank == this.rank && otherC.suit == (this.suit);
     }
 
     public Card clone(){
-        return new Card(suit, value);
+        return new Card(this.suit, this.rank);
     }
 
-    public String getRank(){}
 
 
 }
